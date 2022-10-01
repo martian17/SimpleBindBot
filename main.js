8625d27d673e353fcd3db2d03442c70ced9fc994
martian17 <yakumodanshi@gmail.com>
2022-10-01 22:53:47 +0900

    switch to use http for nginx

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const https = require("https");
+const http = require("http");
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const EventBus = require("./event-bus.js");
 app.use(bodyParser.text({type: '*/*'}));
 
+app.get("/ping",(req,res)=>{
+    res.send("pong\n");
+});
+
 
 //https://smsbind.martian17.com:3265/webhook
-const server = https.createServer({
-    key: fs.readFileSync('./ssl/privkey.pem'),
-    cert: fs.readFileSync('./ssl/fullchain.pem'),
-},app).listen(process.env.PORT, function(){
+//const server = https.createServer({
+//    key: fs.readFileSync('./ssl/privkey.pem'),
+//    cert: fs.readFileSync('./ssl/fullchain.pem'),
+//},app).listen(process.env.PORT, function(){
+//    console.log(`listening to ${process.env.PORT}`);
+//});
+
+const server = http.createServer(app).listen(process.env.PORT,function(){
     console.log(`listening to ${process.env.PORT}`);
 });
+
 
 
 const hub = new EventBus();
